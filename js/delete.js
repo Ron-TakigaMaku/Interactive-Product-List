8217fdaf94519990df9ac26bdea0520000ca0b9c
Yevrey <boyarskiykirill1@gmail.com>
2026-05-25 15:28:59 +0300

    refactor:remove redundant helper functions file

--- js/delete.js
+++
@@ -1,6 +0,0 @@
-container.addEventListener('click', event => {
-	if (!event.target.classList.contains('card__btn')) return
-	const id = Number(event.target.dataset.id)
-	products = products.filter(product => product.id !== id)
-	renderProducts()
-})
